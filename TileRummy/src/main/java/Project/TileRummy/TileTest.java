dcce3fbb4d14f26d0bc0603371da92bf5dff09d8
darrenzel <darrenbian@cmail.carleton.ca>
2018-10-08 16:29:41 -0400

    added test for Tile class

---
+++ TileRummy/src/main/java/Project/TileRummy/TileTest.java
@@ -0,0 +1,16 @@
+package Project.TileRummy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TileTest {
+
+	@Test
+	void test() {
+		Tile tile = new Tile(3,"green");
+		String tileString = "green 3";
+		assertEquals(tileString, tile.toString());
+	}
+
+}
