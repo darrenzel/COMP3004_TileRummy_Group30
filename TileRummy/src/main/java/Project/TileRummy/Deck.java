54c81ab6049d72f3a0ca58ece7e71007168cdb1d
darrenzel <darrenbian@cmail.carleton.ca>
2018-10-08 18:46:39 -0400

    added test for dealTile

--- TileRummy/src/main/java/Project/TileRummy/Deck.java
+++ TileRummy/src/main/java/Project/TileRummy/Deck.java
@@ -3,12 +3,14 @@ import java.util.*;
 public class Deck {
 	final String[] colors = {"green", "red", "black","blue"};
 	 List<Tile> deck = new ArrayList<Tile>();
-
+	 int size;
 	Deck(){
 		
 		for(int i=0; i<colors.length; i++) {
 			for(int j=1; j<14; j++) {
 				deck.add(new Tile(j,colors[i]));
+				deck.add(new Tile(j,colors[i]));
+				size+=2;
 			}
 		}
 	}
--- TileRummy/src/main/java/Project/TileRummy/DeckTest.java
+++ TileRummy/src/main/java/Project/TileRummy/DeckTest.java
@@ -7,12 +7,20 @@ import org.junit.jupiter.api.Test;
 class DeckTest {
 
 	@Test
-	void test() {
+	void testshuffle() {
+		//compare card at the same index before and after the shuffle
 		Deck deck = new Deck();
 		Tile a = deck.deck.get(1);
 		deck.shuffle();
 		Tile b = deck.deck.get(1);
 		assertNotSame(a,b);
+		//test if deck has 104 cards
+		assertEquals(104,deck.size);
 	}
-
+	void testDealTile() {
+		Deck deck = new Deck();
+		Tile a = deck.deck.get(0);
+		Tile b = new Tile(1,"green");
+		assertEquals(a,b);
+	}
 }
