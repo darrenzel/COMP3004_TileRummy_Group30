4b92cca27652fade3744088283bf290951752392
chengyangliu47 <35850718+chengyangliu47@users.noreply.github.com>
2018-10-14 16:14:09 -0400

    player.class

--- TileRummy/src/main/java/Project/TileRummy/player.java
+++ TileRummy/src/main/java/Project/TileRummy/player.java
@@ -1,5 +1,50 @@
 package Project.TileRummy;
 
-public class player {
+import java.util.*;
+import java.util.Map.Entry;
+
+public abstract class player {
+	public ArrayList<Tile> hand;
+	public String name;
+	public int index;
+	
+	public player(ArrayList<Tile> hand, String name, int index)
+	{
+		this.hand = hand;
+		this.name = name;
+		this.index = index;
+	}
+	
+	public  abstract  HashMap<Integer,ArrayList<Integer>> play();
+	
+	
+	public Boolean checkValidity(Map<Integer,ArrayList<Integer>> table)
+	{
+		for(Map.Entry<Integer, ArrayList<Integer>> meld:table.entrySet())
+		{
+			for(int tileIndex = 0; tileIndex < table.get(meld).size() - 1; tileIndex++)
+			{
+				if(table.get(meld).get(tileIndex) != table.get(meld).get(tileIndex) ||
+					table.get(meld).get(tileIndex) != table.get(meld).get(tileIndex + 1))
+				{
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	Boolean winCheck();
+	Boolean endTurn();
+	
+	@Override
+	public String toString()
+	{
+		return this.hand + " " + this.name + " " + this.index;
+	}
 
 }
