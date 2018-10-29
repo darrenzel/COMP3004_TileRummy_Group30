d110dc5cf67eea337f4e7e3ec9f63a060a97d7be
LydiaJiaruiLi <JIARUILI@CMAIL.CARLETON.CA>
2018-10-29 18:18:29 -0400

    update Table.java and TableTest.java
    complete the toString() function and the test for it

--- TileRummy/src/main/java/Project/TileRummy/Table.java
+++ TileRummy/src/main/java/Project/TileRummy/Table.java
@@ -1,17 +1,25 @@
 package Project.TileRummy;
 
 import java.util.ArrayList;
-
+import java.util.Set;
 import java.util.HashMap;
 import java.util.Observable;
 
 
 public class Table extends Observable {
+	int row;
+	ArrayList<Tile> tileSet;
 	public HashMap<Integer, ArrayList<Tile>> table;
+	
+	public Table(){
+		table = new HashMap<Integer, ArrayList<Tile>>();
+	}
+	
 
 	public void clearTable() {
 		table.clear();
 	}
+	
 	
 	public void notifyObserver(HashMap<Integer, ArrayList<Tile>> table){
 		this.table = table;
@@ -19,7 +27,21 @@ public class Table extends Observable {
 	}
 	
 	
-	public String toString() {		
-		return " ";
+	public String tableToString() {
+		Set<Integer> keys = table.keySet();
+		String row = "";
+		String tileString = "";
+		
+		for(int i: keys){
+            row = "Row " + i +": ";
+            tileString = "";
+            ArrayList<Tile> tList = table.get(i);
+            for(Tile t : tList) {
+                tileString += t.toString() +" ";
+            }
+            return row + tileString;
+        }
+		return row + tileString;
 	}
+	
 }
