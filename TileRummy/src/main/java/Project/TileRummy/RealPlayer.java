4527026baa86ed7701e52f7135cc23ce826275a4
chengyangliu47 <35850718+chengyangliu47@users.noreply.github.com>
2018-11-04 14:47:47 -0500

    Update 1.0

---
+++ TileRummy/src/main/java/Project/TileRummy/RealPlayer.java
@@ -0,0 +1,15 @@
+package Project.TileRummy;
+mport java.util.Scanner;
+import java.util.Map.Entry;
+
+public class RealPlayer extends Player {
+	
+	public RealPlayer(Player p)
+	{
+		super(p);
+	}
+	
+	public Table play(Table table){
+		
+	}
+}
