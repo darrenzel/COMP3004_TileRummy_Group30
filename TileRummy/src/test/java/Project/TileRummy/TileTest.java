517f09f57009e3f692ab9dbd16f315651bc06b1c
darrenzel <darrenbian@cmail.carleton.ca>
2018-11-05 05:14:30 -0400

    Final Version

--- TileRummy/src/test/java/Project/TileRummy/TileTest.java
+++ TileRummy/src/test/java/Project/TileRummy/TileTest.java
@@ -1,9 +1,56 @@
 package Project.TileRummy;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class TileTest extends TestCase {
-	Tile tile = new Tile(3,"green");
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+class TileTest {
+
+	@Test
+	void testtoSting() {
+		//test toString method
+		Tile tile = new Tile(3,"green");
+		String tileString = "green 3";
+	assertEquals(tileString, tile.toString());
 	
-	assertSame()
+	
+	
+	}
+	@Test
+	void testColorComparator() {
+		//test color comparator
+		Tile tile = new Tile(3,"green");
+		Tile tile2 = new Tile(5,"blue");
+		ArrayList<Tile> hand = new ArrayList<Tile>();
+		ArrayList<Tile> expected = new ArrayList<Tile>();
+		
+		expected.add(tile2);
+		expected.add(tile);
+		
+		hand.add(tile);
+		hand.add(tile2);
+		Collections.sort(hand, Tile.colorComparator);
+		
+		assertEquals(expected, hand);
+	}
+	@Test
+	void testValueComparator() {
+		//test color comparator
+		Tile tile = new Tile(3,"green");
+		Tile tile2 = new Tile(5,"blue");
+		ArrayList<Tile> hand = new ArrayList<Tile>();
+		ArrayList<Tile> expected = new ArrayList<Tile>();
+		
+		expected.add(tile);
+		expected.add(tile2);
+		
+		hand.add(tile2);
+		hand.add(tile);
+		Collections.sort(hand, Tile.valueComparator);
+		
+		assertEquals(expected, hand);
+	}
 }
